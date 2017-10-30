68599809b7cef60a48e5847697a97253a1403050
Khurshid Wali <khurshid135@hotmail.com>
2017-10-30 22:33:35 +0500

    New movie add through form functionality added
    available at /add

--- Myflix/Controllers/HomeController.cs
+++ Myflix/Controllers/HomeController.cs
@@ -43,6 +43,29 @@ namespace myflix.Controllers
             return View();
         }
 
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add([Bind(Include =
+"uid,imid,name,language,year,Genre1, Genre2, Genre3,Resolution,Image,cast1,cast2,cast3,cast4,cast5")] Movy movy)
+        {
+            myflixDBEntities entities = new myflixDBEntities();
+            if (ModelState.IsValid)
+            {
+                movy.Views = 0;
+                movy.Image = movy.Name + movy.Year + ".jpg";
+                entities.Movies.Add(movy);
+                entities.SaveChanges();
+                return RedirectToAction("Add");
+            }
+
+            return View(movy);
+
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
